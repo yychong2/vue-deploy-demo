57bfb7c406e00a25b3a9af5bb81fcb7ea5e15471
Yy <yychong2022@gmail.com>
2023-05-03 17:12:47 +0800

    fix path typo

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-deploy-demo/index',
+  base: '/vue-deploy-demo/index/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
