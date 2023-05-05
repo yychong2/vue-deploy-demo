f39140840d6469fb55ee6e675a2286d2ea270b20
yychong2 <117072512+yychong2@users.noreply.github.com>
2023-05-05 12:35:08 +0800

    update global url in main js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
+import { createApp, VueElement } from 'vue'
 import App from './App.vue'
-
 // import './assets/main.css'
+
 import './assets/css/style.css'
 import './assets/vendor/bootstrap/css/bootstrap.min.css'
 import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
@@ -10,4 +10,10 @@ import './assets/vendor/swiper/swiper-bundle.min.css'
 
 
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.config.globalProperties = {
+    apiUrl: "https://flut.jcmmweb.com/api/v1/",
+
+};
+
+app.mount('#app')
