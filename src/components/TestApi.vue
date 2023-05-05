1c296c0bbdec029678e9616fb3e351b8d7993e47
yychong2 <117072512+yychong2@users.noreply.github.com>
2023-05-05 10:49:18 +0800

    update testapi.vue remove sensitive data

--- src/components/TestApi.vue
+++ src/components/TestApi.vue
@@ -86,61 +86,19 @@ export default {
             });
         },
         getToken() {
-            axios.post('https://flut.jcmmweb.com/api/v1/auth ', {   
-                username: "davis7231",
-                password: '123qwe',
-            },
-            {  
-                'X-Requested-With': 'XMLHttpRequest',
-                 withCredentials: true
-            }
-            )
-            .then(response => {
-              this.data = response.data;
-              this.token = response.data.AccessToken;
-              this.memDetail = response.data.MemberDetail;
-              //console.log(response.headers['set-cookie']);
-              //console.log(JSON.stringify(this.memDetail));
-              //console.log(toRaw(this.memDetail));
-              // this.GetProductListByCategory();
-            })
-            .catch(error => {
-              console.error(error);
-            });
+            
         },
         greet() {
             console.log(this.message);
         },
         GetProductListByCategory(){
-            console.log(this.token)
-            axios.post('https://flut.jcmmweb.com/api/v1/GetProductListByCategory', {   
-                  "CategoryType": "SL",
-                  "IsLaunchGame": false
-                }
-            )
-            .then(response => {
             
-              this.gameCasino = response.data.ProductTypeContent;
-              console.log(response.data.ProductTypeContent);
-            })
-            .catch(error => {
-              console.error(error);
-            });
         },
         GetUserProfile(){
             
-            // const headers = { 
-            //     "Content-Type": "application/json",
-            //     "Cookie" : "im1mYKDxV8SfewHRyCIKYOF4QYCkFxBAd4wNcCzUTgcTAg6Fj6sugZIuQ7GWHNrQfq2g2QaT/eHTfuhtdmblVg==",
-            //     "Language":"en-US",
-            //     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjE0NSIsImp0aSI6Ijk4OGQwOWE0LTNjNWYtNDAzNy1iYTBhLWZkNGY5YTJkNGQ5ZCIsIm5iZiI6MTY4MzAxMzQ3MiwiZXhwIjoxNjgzMDM1MDcyLCJpYXQiOjE2ODMwMTM0NzJ9.k4NCpjVhc--wLexqomqzCzdPt08w4XLmQCmfMPI3Log" ,
-            //     "X-Member-Details": '{ "Username": "local01", "UserId": "12145","UserGroup": "42","UserGroupName": "VIP42","Email": "laliho@mail.com","IpAddress": "161.142.151.114","IsPauseBet": false,"PlayerType": "0","AgentId": "1","AgentCode": "ag1","Name": "Laliho","DateCreated": "2022-11-10T08:27:59","MinWithdrawal": 0,"MaxWithdrawal": 0,"DailyWithdrawalLimit": 0,"DailyWithdrawalLimitAmt": 0,"Currency": "CNY"}' ,
-            // };
-        
-            //var memDetail = '{ "Username": "local01", "UserId": "12145","UserGroup": "42","UserGroupName": "VIP42","Email": "laliho@mail.com","IpAddress": "161.142.151.114","IsPauseBet": false,"PlayerType": "0","AgentId": "1","AgentCode": "ag1","Name": "Laliho","DateCreated": "2022-11-10T08:27:59","MinWithdrawal": 0,"MaxWithdrawal": 0,"DailyWithdrawalLimit": 0,"DailyWithdrawalLimitAmt": 0,"Currency": "CNY"}';
+            
             axios.defaults.headers.common['X-Member-Details'] = JSON.stringify(this.memDetail);
             axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.token;
-            //axios.defaults.headers.common['Cookie'] = 'xGWodtdScUzVe+ySXuMDV6OMGzvz0IbJq8mBLP6oXgKeHlzxwgLwLvbySMICFeBBYw9z3gFChakPH1PWkKniAw==';
 
             const headers = { 
                 "Content-Type": "application/json",
