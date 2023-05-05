edc1110a4c470fca683802372bf22a31f9d6f1f2
yychong2 <117072512+yychong2@users.noreply.github.com>
2023-05-05 12:24:26 +0800

    update test api vue login part

--- src/components/TestApi.vue
+++ src/components/TestApi.vue
@@ -1,29 +1,52 @@
 <template>
-    <h3 class="title">Test API</h3>
-    <div>
-        <!-- <p>{{ indexes }}</p> -->
-        <button @click="getToken">Click Token and GameList</button>
-    </div>
-  
-    <div :style="classReadData" style="overflow-wrap: break-word" >
-        <!-- <p>{{ data }}</p> -->
+    <h3 class="title">{{ title }} </h3>
+
+    <form @submit.prevent="loginWithPassword">
+      <label>
+        Username
+        <input type="text" v-model="username" />
+      </label>
+      <label>
+        Password
+        <input type="password" v-model="password" />
+      </label>
+      <button type="button" @click="getToken">Log in</button>
+    </form>
+
+
+    <!-- <div>
+        <p>{{ indexes }}</p> 
+        <button @click="getToken">Click Token</button>
+    </div> -->
+    <Transition>
+    <div v-if="tokenRes">
+
+        <div style="overflow-wrap: break-word" >
         <p>Token : {{ token }}</p>
-    </div>
+        </div>
 
-  
-     <!-- <p>{{ data }}</p> -->
-    <div class="row">
-    <div class="column" v-for="(item,index) of gameCasino" :key="index">
-        <h4 class="title">{{ item.ProductName }}</h4>
-        <span><img :style="imagaSize" :src="item.Image2 "/></span>
-    </div >
-    </div>
+        <div>
+            <!-- <p>{{ indexes }}</p> -->
+            <button type="button" @click="GetUserProfile">Click Profile Detail after click Token</button>
+            <p>Profile Detail :{{ memProfile }}</p>
+        </div>
 
-    <div>
-        <!-- <p>{{ indexes }}</p> -->
-        <button @click="GetUserProfile">Click Profile Detail</button>
-        <p>Profile : {{ userProfile }}</p>
+        <div>
+            <button @click="GetProductListByCategory">Click get GameList</button>
+            <div class="row">
+            <div class="column" v-for="(item,index) of gameCasino" :key="index">
+                <h4 class="title">{{ item.ProductName }}</h4>
+                <span><img :style="imagaSize" :src="item.Image2 "/></span>
+            </div>
+            </div>
+        </div>
     </div>
+    <div v-else>
+        <p>{{ errorMsg }}</p> 
+       
+    </div>
+    </Transition>
+
 
   
 </template>
@@ -32,27 +55,27 @@
 import axios from 'axios';
 import { isProxy, toRaw } from 'vue';
 
-axios.defaults.withCredentials = false;
+axios.defaults.withCredentials = true;
 
 export default {
     data(){
         return{
-            data :{
-                type : String
+            data :"",
+            token:"",
+            username:"",
+            password:"",
+            tokenRes : false,
+            title:"",
+            errorMsg : "",
+            gameCasino:[],
+            memDetail:{},
+            imagaSize:{
+                width:"100px"
             },
-            token:{
-                type : String
-            },
-            userProfile:{type : String},
             classReadData:{
                 width: "796px",
             },
-            gameCasino:[],
-            memDetail:{},
-            message: 'Hello, world!',
-            imagaSize:{
-                width:"200px"
-            }
+            memProfile:{}
         }
     },
     setup(){
@@ -65,51 +88,93 @@ export default {
         }
     },
     created(){
-    //    this.indexes();
+        //  console.log(this.apiUrl);
+            this.indexes();
     },
     methods: {
         indexes :function(params) {
             console.log("indexes");
-            
-            axios.post('https://flut.jcmmweb.com/api/v1/auth', {   
-                username: "",
-                password: '',
+            this.title = "Welcome Vue Demo"
+
+            // axios.post(this.apiUrl+ 'auth', {   
+            //     username: "test001",
+            //     password: '123qwe',
+            // }
+            // )
+            // .then(response => {
+            //   console.log(response.data);
+            //   this.data = response.data;
+            //   this.token = response.data.AccessToken;
+            // })
+            // .catch(error => {
+            //   console.error(error);
+            // });
+        },
+        getToken() {
+         
+            axios.post( this.apiUrl +'auth ', {   
+                username: this.username,
+                password: this.password,
+            },
+            {  
+                'X-Requested-With': 'XMLHttpRequest',
+                withCredentials: true,
+                credentials: 'include'
             }
             )
             .then(response => {
-              console.log(response.data);
               this.data = response.data;
               this.token = response.data.AccessToken;
+              this.memDetail = response.data.MemberDetail;
+
+              //console.log(toRaw(this.memDetail));
+              this.tokenRes = true;
+
+            })
+            .catch(error => {
+                console.error(error);
+                this.errorMsg = error;
+            });
+        },
+        
+        GetProductListByCategory(){
+            console.log(this.token)
+            axios.post(this.apiUrl+ 'GetProductListByCategory', {   
+                  "CategoryType": "SL",
+                  "IsLaunchGame": false
+                }
+            )
+            .then(response => {
+            
+              this.gameCasino = response.data.ProductTypeContent;
+              console.log(response.data.ProductTypeContent);
             })
             .catch(error => {
               console.error(error);
             });
         },
-        getToken() {
-            
-        },
-        greet() {
-            console.log(this.message);
-        },
-        GetProductListByCategory(){
-            
-        },
+
         GetUserProfile(){
+          
+            var memDetail2 = JSON.stringify(toRaw(this.memDetail));
+            //console.log(memDetail2)
             
-            
-            axios.defaults.headers.common['X-Member-Details'] = JSON.stringify(this.memDetail);
-            axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.token;
+            axios.defaults.headers.common['Content-Type'] = "application/json";
+            axios.defaults.headers.common['X-Member-Details'] = memDetail2;
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' +  this.token;
 
             const headers = { 
                 "Content-Type": "application/json",
                 "Language":"en-US",
+                "X-Member-Details" : axios.defaults.headers.common['X-Member-Details'],
+                "Authorization" : axios.defaults.headers.common['Authorization']
             };
-            
-            //console.log(headers);
-            axios.get('https://flut.jcmmweb.com/api/v1/GetUserProfile', { withCredentials: true} ,{headers}
+
+            axios.get('https://flut.jcmmweb.com/api/v1/GetUserProfile',
+             {  } ,   {headers}
             ).then(response => {
               console.log(response.data);
-              this.userProfile = response.data;
+              this.memProfile = response.data.UserDetail
             })
             .catch(error => {
               console.error(error);
@@ -117,7 +182,8 @@ export default {
         }
     },
     computed:{
-    }
+    },
+   
 };
 
 
@@ -152,4 +218,21 @@ export default {
   padding: 5px;
 }
 
+.active{
+    display: block;
+}
+
+.non-active {
+    display: none;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
 </style>
