0b8a6657643924748377d0742f6ce4d2b1266335
mone7231 <mone7231@users.noreply.github.com>
2023-05-09 09:09:29 +0000

    deploy: 06e3f1697c99e127f35b4fb23b6182f98c3210bb

---
+++ assets/NotFound-b9336c2a.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c as t}from"./index-f23e7664.js";const n={},_={class:"fontColour"};function c(s,r){return e(),t("h3",_,"404 Not Found")}const u=o(n,[["render",c]]);export{u as default};
